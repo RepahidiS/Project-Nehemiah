49098c7e97576db454af49f28c87f8829101469e
Andrew54757 <andrews54757@gmail.com>
2016-08-16 21:22:47 +0000

    Update shellService.js

--- source/core/shellService.js
+++ source/core/shellService.js
@@ -1,1 +1,30 @@
 
+module.exports = class ShellService {
+constructor(controlservice) {
+  this.controller = controlservice
+  
+}
+init() {
+    var readline = require('readline');
+    var readlinein = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    setTimeout(this.prompt(readlinein), 1);
+}
+
+
+prompt(in_) {
+    in_.question(">", function(str) {
+        this.parseCommands(str);
+        return this.prompt(); // Too lazy to learn async
+    });
+}
+
+parseCommands(str) {
+    controller.logger.onCommand(str);
+    if (str === '')
+        return;
+controller.execCommand
+}
+}
