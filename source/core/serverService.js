808ba283cce8a45bfae4e0c8cce3dc745ccd873c
Andrew54757 <andrews54757@gmail.com>
2016-08-16 21:52:05 +0000

    Update serverService.js

--- source/core/serverService.js
+++ source/core/serverService.js
@@ -1,1 +1,14 @@
-
+"use strict";
+const Main = require('./main.js')
+module.exports = class ServerService {
+  constructor(controller,globalData) {
+    this.globalData = globalData
+    this.controller = controller
+    var serv = new Main(true,globalData)
+  }
+  init() {
+    
+  }
+  
+  
+}
