e1a719c46c25d1310b014366f2427ac66f132039
Andrew54757 <andrews54757@gmail.com>
2016-08-16 21:45:29 +0000

    Update main.js

--- source/core/main.js
+++ source/core/main.js
@@ -1,1 +1,21 @@
-
+"use strict"
+const DataService = require('./dataService.js')
+module.exports = class Main {
+  constructor(isMain,globalData) {
+    this.isMain = isMain;
+    this.dataService = new DataService(this,globalData)
+    
+  }
+  init() {
+    
+    
+  }
+  stop() {
+    
+  }
+  pause() {
+    
+  }
+  
+  
+}
