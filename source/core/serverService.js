8334932ca717fc77440fd989efe32aa1d30eea21
Andrew54757 <andrews54757@gmail.com>
2016-08-16 21:58:36 +0000

    Update serverService.js

--- source/core/serverService.js
+++ source/core/serverService.js
@@ -4,11 +4,30 @@ module.exports = class ServerService {
   constructor(controller,globalData) {
     this.globalData = globalData
     this.controller = controller
-    var serv = new Main(true,globalData)
+    
+    var serv = new Main(true,0,globalData)
+    this.servers = [];
+    this.ids = 0
+    this.servers[0] = serv;
+    this.selected = serv;
+    serv.init()
+  }
+  getNextId() {
+    return this.ids ++;
   }
   init() {
     
   }
-  
+  getCommand() {
+    
+    
+  }
+  execCommand(str) {
+    var cmd = str.split(" ");
+    
+    
+    if (!this.selected) return;
+    
+  }
   
 }
