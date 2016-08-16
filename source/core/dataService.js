1066423ba271d276fedcb94c0c441c6d5025d478
Andrew54757 <andrews54757@gmail.com>
2016-08-16 21:41:56 +0000

    Update dataService.js

--- source/core/dataService.js
+++ source/core/dataService.js
@@ -1,1 +1,13 @@
-
+module.exports = class DataService {
+  constructor(main,globalData) {
+    this.globalData = globalData;
+    this.gameData = new GameData();
+    this.world = new World()
+    
+    
+  }
+  
+  
+  
+  
+}
