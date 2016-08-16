8f99794e80e5148e11b49a87c9022e7da64c4fe6
Andrew54757 <andrews54757@gmail.com>
2016-08-16 21:26:21 +0000

    Update controller.js

--- source/core/controller.js
+++ source/core/controller.js
@@ -1,1 +1,19 @@
-
+"use strict"
+const ShellService = require('./shellService.js')
+const ServerService = require('./serverService.js')
+module.exports = class Controller {
+  constructor() {
+    
+    
+    
+    this.shellService = new ShellService(this)
+    this.serverService = new ServerService(this)
+  }
+  execCommand(str) {
+    this.serverService.execCommand(str)
+    
+  }
+  
+  
+  
+}
