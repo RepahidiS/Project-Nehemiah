26923364ad24d1433112c1a950768315e6168b21
Andrew54757 <andrews54757@gmail.com>
2016-08-16 21:32:49 +0000

    Update cell.js

--- source/entities/cell.js
+++ source/entities/cell.js
@@ -1,1 +1,17 @@
+"use strict";
 
+module.exports = class Cell {
+  constructor(id,position,mass,ownerid,other) {
+  this.id = id
+  this.position = position
+  this.mass = mass;
+  this.ownerid = ownerid
+  this.other = other
+  
+  }
+  onCreation(dataService) {
+    
+  }
+  
+  
+}
