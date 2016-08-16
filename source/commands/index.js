22fba9d1f38644b2f872fa458e9e4864e30560aa
Andrew54757 <andrews54757@gmail.com>
2016-08-16 22:09:08 +0000

    Update index.js

--- source/commands/index.js
+++ source/commands/index.js
@@ -1,1 +1,31 @@
-
+module.exports = class Commands {
+  constructor() {
+    this.list = {
+      
+      
+    }
+    this.serverService = {
+      create: function(serverService,args) {
+      
+        
+        serverService.create()
+      }
+      
+      
+      
+    }
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
