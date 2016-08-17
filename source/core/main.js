d76a8cb346e4ac0da72eb7fb0502bdf3a62ed498
Andrew54757 <andrews54757@gmail.com>
2016-08-17 13:33:22 +0000

    Update main.js

--- source/core/main.js
+++ source/core/main.js
@@ -1,8 +1,11 @@
 "use strict"
 const DataService = require('./dataService.js')
 module.exports = class Main {
-  constructor(isMain,globalData) {
+  constructor(isMain,id,name,scname,globalData) {
     this.isMain = isMain;
+    this.id = id
+    this.name = name
+    this.scname = scname
     this.dataService = new DataService(this,globalData)
     
   }
