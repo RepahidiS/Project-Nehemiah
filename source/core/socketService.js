f8b632a4d154059d88252dd6ba04a9f371a73d46
Andrew54757 <andrews54757@gmail.com>
2016-09-05 18:59:03 -0400

    Update socketService.js

--- source/core/socketService.js
+++ source/core/socketService.js
@@ -1,1 +1,10 @@
+"use strict"
 
+module.exports = class socketService {
+  constructor(globalData) {
+    this.globalData = globalData;
+this.io = require("socket.io")
+this.server = this.io(globalData.config.port)
+  }
+ 
+}
